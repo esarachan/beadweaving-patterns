e0ef53d0ab10f1dc89e3731bdaac84e9ce288825
Emma Sarachan <emmasarachan@gmail.com>
2020-04-04 19:06:58 -0400

    Compute width of screen in chars

--- src/components/common/WindowResizeWatcher.tsx
+++ src/components/common/WindowResizeWatcher.tsx
@@ -1,12 +1,71 @@
 import * as React from "react";
+import times from "lodash.times";
+
+type CanaryLineProps = {
+  onChangeLength: (length: number) => void;
+};
+
+type CanaryLineState = {
+  length: number;
+};
+
+class CanaryLine extends React.Component<CanaryLineProps, CanaryLineState> {
+  domElement: any;
+
+  constructor(props: CanaryLineProps) {
+    super(props);
+    this.state = { length: 0 };
+    this.domElement = null;
+  }
+
+  componentDidMount() {
+    this.handleWindowResize();
+    window.addEventListener("resize", this.handleWindowResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleWindowResize);
+  }
+
+  isOverflowing = () =>
+    this.domElement.scrollWidth > this.domElement.clientWidth;
+
+  setStateP = async (state: CanaryLineState) =>
+    new Promise((resolve) => this.setState(state, resolve));
+
+  handleOverflow = async () => {
+    const maxLength = this.state.length - 1;
+    await this.setStateP({ length: maxLength });
+    this.props.onChangeLength(maxLength);
+  };
+
+  handleWindowResize = async () => {
+    await this.setStateP({ length: 0 });
+    while (!this.isOverflowing()) {
+      await this.setStateP({ length: this.state.length + 1 });
+    }
+    await this.handleOverflow();
+  };
+
+  setDOMElement = (domElement: any) => {
+    this.domElement = domElement;
+  };
+
+  render() {
+    return (
+      <div ref={this.setDOMElement} className="ascii-art canary-line">
+        {times(this.state.length, () => " ")}
+      </div>
+    );
+  }
+}
 
 type WindowResizeWatcherProps = {
   children: React.ReactNode;
 };
 
 type WindowResizeWatcherState = {
-  width: number;
-  height: number;
+  widthInChars: number;
 };
 
 class WindowResizeWatcher extends React.Component<
@@ -15,29 +74,23 @@ class WindowResizeWatcher extends React.Component<
 > {
   constructor(props: WindowResizeWatcherProps) {
     super(props);
-    this.state = { width: 0, height: 0 };
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    this.state = { widthInChars: 0 };
   }
 
-  componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener("resize", this.updateWindowDimensions);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateWindowDimensions);
-  }
-
-  updateWindowDimensions() {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
-  }
+  handleChangeWidth = (widthInChars: number) => this.setState({ widthInChars });
 
   render() {
-    return React.Children.toArray(this.props.children).map((child) =>
-      React.cloneElement(child as React.ReactElement<any>, {
-        containerWidth: this.state.width,
-        containerHeight: this.state.height,
-      })
+    const transformedChildren = React.Children.toArray(this.props.children).map(
+      (child) =>
+        React.cloneElement(child as React.ReactElement<any>, {
+          containerWidthChars: this.state.widthInChars,
+        })
+    );
+    return (
+      <React.Fragment>
+        <CanaryLine onChangeLength={this.handleChangeWidth} />
+        {transformedChildren}
+      </React.Fragment>
     );
   }
 }
